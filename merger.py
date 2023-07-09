26788b42c1f5431a9715910df2dff89a646b5ff5
unknown <philip901203777@gmail.com>
2023-07-09 14:05:37 +0800

    [new function] can specify the merging order

--- merger.py
+++ merger.py
@@ -1,13 +1,34 @@
 from pypdf import PdfWriter
 import os
 
+# merger
 merger = PdfWriter()
-# merger = PyPDF.PdfFileMerger()
+
+# directory of the original PDF files
 dir_original = "./pdf_original"
 
-for file_name in os.listdir(dir_original):
+# list of file names
+name_list = []
+
+print("Find PDF files:")
+# enumerate through the origional directory
+for idx, file_name in enumerate(os.listdir(dir_original)):
     if file_name.endswith(".pdf"):
-        dir_file = os.path.join(dir_original, file_name)
-        merger.append(dir_file)
+        print(f'{idx+1}. {file_name}')
+        name_list.append(file_name)
+
+# input the merging order
+file_order = input ("Enter the Merging Order (ex: 1, 3, 2, ...):")
+file_order = file_order.split(",")
+
+# check input length
+if(len(file_order)!=len(name_list)):
+    raise Exception("Invaild Merging Order!")
+
+# merge file according to the order
+for index in file_order:
+    dir_file = os.path.join(dir_original, name_list[int(index)-1])
+    merger.append(dir_file)
 merger.write("./pdf_merged/merged-pdf.pdf")
 merger.close()
+print("Successfully Merge!")
